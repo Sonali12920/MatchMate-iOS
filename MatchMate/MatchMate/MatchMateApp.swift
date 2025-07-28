2807a3583ae378125a54797f386c357e75bfed49
Sonali2921 <sonali.na@oyorooms.com>
2025-07-28 23:12:26 +0530

    Initial project setup with clean structure

---
+++ MatchMate/MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Sonali on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
